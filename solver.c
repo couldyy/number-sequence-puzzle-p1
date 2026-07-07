303441225157b84fc8843162d3d411c179f2c7e1
couldyy <couldy111@gmail.com>
2026-07-07 19:19:56 +0300

    v2. Connect nodes via simple hashtable. Move files aroung.
    Each node has 'nodes_out' dynamic array, that contains all elements,
    whose 'key_start' == 'key_end' (current_node).

--- solver.c
+++ solver.c
@@ -6,14 +6,14 @@
 #include <string.h>
 
 #define MARENA_IMPLEMENTATION
-#include "marena.h"
+#include "thirdparty/marena.h"
 
 /*
     TODO: check whether there are path with equal length but different nodes. What to do in this case?
     - rename 'sibling' to something more apropriate
 
     FIXME:
-        - allocs all of the possible memory, find cause
+        [*] - allocs all of the possible memory, find cause
 */
 
 #define NUMBER_WIDTH 6
@@ -78,13 +78,21 @@ Arena g_arena = {0};
     } while (0)
     
 
+typedef struct Number Number;
+
 typedef struct {
-    long start_key;
-    long end_key;
+    Number** items;
+    size_t count;
+    size_t capacity;
+} Number_ptr_array;
+
+typedef struct Number {
+    long key_start;
+    long key_end;
     long num_full;
     bool is_used;
     //Number_array nodes_in;
-    //Number_array nodes_out;
+    Number_ptr_array nodes_out;
 } Number;
 
 typedef struct {
@@ -94,14 +102,65 @@ typedef struct {
 } Number_array;
 
 typedef struct {
-    Number** items;
     size_t count;
     size_t capacity;
-} Number_ptr_array;
+    Number_ptr_array* buckets;
+} Buckets_table;
+
+
+void buckets_free(Buckets_table* buckets_table)
+{
+    assert(buckets_table != NULL);
+    for (size_t i = 0; i < buckets_table->capacity; i++) {
+        Number_ptr_array* current_bucket = &(buckets_table->buckets[i]);
+        if (current_bucket->items != NULL) {
+            da_free(current_bucket);
+        }
+    }
+    free(buckets_table->buckets);
+}
+
+
+void buckets_fill(Buckets_table* key_buckets, Number_array numbers)
+{
+    assert(key_buckets != NULL);
+    assert(numbers.items != NULL);
+    assert(key_buckets->capacity >= numbers.count);
+    
+    for (size_t i = 0; i < numbers.count; i++) {
+        Number* current_number = &(numbers.items[i]);
+        size_t index = current_number->key_start;   // index must be < bucket capacity, but it's fine, since it is checked via assert() previously
+        Number_ptr_array* bucket = &(key_buckets->buckets[index]);
+        da_append(bucket, current_number);
+    }
+}
+
+void nodes_connect_by_key(Number_array numbers)
+{
+    assert(numbers.items != NULL); 
+
+    Buckets_table buckets_table = {0};
+    buckets_table.buckets = calloc(numbers.count, sizeof(Number_ptr_array));
+    assert(buckets_table.buckets != NULL && "calloc() failed");
+    buckets_table.capacity = numbers.count;
+
+    buckets_fill(&buckets_table, numbers);
+
+    for (size_t i = 0; i < numbers.count; i++) {
+        Number* current_number = &(numbers.items[i]);
+        Number_ptr_array* key_bucket = &(buckets_table.buckets[current_number->key_end]);
+        for (size_t j = 0; j < key_bucket->count; j++) {
+            da_append(&(current_number->nodes_out), key_bucket->items[j]);
+        }
+    }
+   
+    buckets_free(&buckets_table);
+}
 
 Number parse_number(char* num_str, size_t num_str_len)
 {
     assert(num_str != NULL);
+
     char* buff_err[num_str_len] = {}; 
     //memset(buff_err, 0, num_str_len);
 
@@ -113,13 +172,13 @@ Number parse_number(char* num_str, size_t num_str_len)
         exit(1);
     }
 
-    long start_key = num / 10000;
-    long end_key = num % 100;
-    return (Number) {
-        .start_key = start_key,
-        .end_key = end_key,
-        .num_full = num,
-    };
+    long key_start = num / 10000;
+    long key_end = num % 100;
+    Number number = {0};
+    number.key_start = key_start;
+    number.key_end = key_end;
+    number.num_full = num;
+    return number;
 }
 
 #define BUFFER_SIZE 1024
@@ -163,12 +222,12 @@ void print_sequence_pretty(Number_ptr_array sequence)
     assert(sequence.items != NULL);
     for (size_t i = 0; i < sequence.count; i++) {
         Number* current_number = sequence.items[i];
-        long num_no_end_key = current_number->num_full / 100;
+        long num_no_key_end = current_number->num_full / 100;
         if (i == sequence.count - 1) {  // last
             printf("%0.*ld\n", NUMBER_WIDTH, current_number->num_full);
         }
         else {
-            printf("%0.*ld", NUMBER_WIDTH - KEY_WIDTH, num_no_end_key);
+            printf("%0.*ld", NUMBER_WIDTH - KEY_WIDTH, num_no_key_end);
         }
     }
 }
@@ -178,7 +237,7 @@ void print_sequence(Number_ptr_array sequence)
     //assert(sequence.items != NULL);
     for (size_t i = 0; i < sequence.count; i++) {
         Number* current_number = sequence.items[i];
-        printf("(%ld) %ld (%ld) --> ", current_number->start_key, current_number->num_full, current_number->end_key);
+        printf("(%ld) %ld (%ld) --> ", current_number->key_start, current_number->num_full, current_number->key_end);
     }
     printf("\n");
 }
@@ -191,7 +250,7 @@ Number_ptr_array find_all_siblings(Number_array number_array, Number* num)
     Number_ptr_array number_ptr_array = {0}; 
     for (size_t i = 0; i < number_array.count; i++) {
         Number* current_number = &(number_array.items[i]);
-        if (current_number->num_full != num->num_full && !(current_number->is_used) && num->end_key == current_number->start_key) {
+        if (current_number->num_full != num->num_full && !(current_number->is_used) && num->key_end == current_number->key_start) {
             da_append(&number_ptr_array, current_number);
         }
     }
@@ -208,38 +267,37 @@ Number_ptr_array find_longest_sequence_for_node(Number_array number_array, Numbe
     
     Number_ptr_array local_longest = {0};
 
-    //Number_ptr_array number_ptr_array = find_all_siblings(number_array, node);
 
-    //for (size_t i = 0, count = 0; i < number_ptr_array.count; i++) {
-    size_t count = 0;
-    for (size_t i = 0; i < number_array.count; i++) {
-        Number* current_number = &(number_array.items[i]);
-        if (0 && current_number->is_used) {
-            printf("%ld in use, depth: %zu\n", current_number->num_full, depth);
+
+    size_t available_next_node_count = 0;
+    for (size_t i = 0; i < node->nodes_out.count; i++) {
+        Number* current_number = node->nodes_out.items[i];
+        if (current_number->is_used) {
+            //printf("%ld in use, depth: %zu\n", current_number->num_full, depth);
+            continue;
         }
-        if (current_number->num_full != node->num_full && !(current_number->is_used) && node->end_key == current_number->start_key) {
-            ++count;
-            current_sequence->count = depth + 1;    // reset array for each sibling 
+        current_sequence->count = depth + 1;    // reset array for each sibling 
+        available_next_node_count++;
 
-            Number_ptr_array longest_for_next_node = find_longest_sequence_for_node(number_array, current_number, current_sequence, depth + 1);
-            if (0 && longest_for_next_node.count == local_longest.count && longest_for_next_node.count >= 66) {
-                printf("------------------------------\n");
-                printf("Equal size paths: %zu\n", longest_for_next_node.count);   // TODO: print paths
-                //print_sequence_pretty(longest_for_next_node);
-                //print_sequence_pretty(local_longest);
-                printf("------------------------------\n");
-            }
-            if (longest_for_next_node.count > local_longest.count) {
-                if (local_longest.items != NULL) da_free(&local_longest);
-                local_longest = longest_for_next_node;
-            }
-            else {
-                da_free(&longest_for_next_node);
-            }
+        Number_ptr_array longest_for_next_node = find_longest_sequence_for_node(number_array, current_number, current_sequence, depth + 1);
+        if (0 && longest_for_next_node.count == local_longest.count && longest_for_next_node.count >= 66) {
+            printf("------------------------------\n");
+            printf("Equal size paths: %zu\n", longest_for_next_node.count);   // TODO: print paths
+            //print_sequence_pretty(longest_for_next_node);
+            //print_sequence_pretty(local_longest);
+            printf("------------------------------\n");
+        }
+        if (longest_for_next_node.count > local_longest.count) {
+            if (local_longest.items != NULL) da_free(&local_longest);
+            local_longest = longest_for_next_node;
+        }
+        else {
+            da_free(&longest_for_next_node);
         }
     }
 
-    if (count == 0) {  // tail
+    // even if there are nodes_out in current number, all of them may already be used, meaning this is an end of sequence  
+    if (node->nodes_out.count == 0 || available_next_node_count == 0) {  // tail
         da_cpy(&local_longest, current_sequence);
         goto ret;
     }
@@ -250,7 +308,6 @@ ret:
 }
 
 
-// TODO: free arrays if they are not longer then current
 Number_ptr_array find_longest_sequence(Number_array number_array)
 {
     assert(number_array.items != NULL && number_array.count > 0);
@@ -271,7 +328,7 @@ Number_ptr_array find_longest_sequence(Number_array number_array)
         da_reset(&current_sequence);    // 'find_longest_sequence_for_node()' reset only to the last added elemend, i.e. it will add nodes on each top-level iteration and NOT remove them
         Number_ptr_array longest_for_node = find_longest_sequence_for_node(number_array, current_number, &current_sequence, 0);
 
-            if (longest_for_node.count == longest_sequence.count && longest_for_node.count >= 0) {
+            if (0 && longest_for_node.count == longest_sequence.count && longest_for_node.count >= 0) {
                 printf("------------------------------\n");
                 printf("Equal size paths: %zu\n", longest_for_node.count);   // TODO: print paths
                 //print_sequence_pretty(longest_for_next_node);
@@ -288,18 +345,54 @@ Number_ptr_array find_longest_sequence(Number_array number_array)
     return longest_sequence;
 }
 
+void print_children(Number_array number_array)
+{
+    Number_ptr_array nodes_out_linear = {0};
+    for (size_t i = 0; i < number_array.count; i++) {
+        da_reset(&nodes_out_linear);
+
+        printf("------------------------------\n");
+        Number* current_number = &(number_array.items[i]);
+        printf("%ld (%ld) |\n", current_number->num_full, current_number->key_end);
+        for (size_t j = 0; j < current_number->nodes_out.count; j++) {
+            printf("\t > %ld\n", current_number->nodes_out.items[j]->num_full);
+        }
+
+        // check whether it is the same as finding linearly
+        if (0) {
+            // Fill linearly
+            for (size_t j = 0; j < number_array.count; j++) {
+                Number* current_node = &(number_array.items[j]);
+                if (current_node->num_full != current_number->num_full && current_number->key_end == current_node->key_start) {
+                    da_append(&nodes_out_linear, current_node);
+                }
+            }
+            printf("++++++++++\n");
+            printf("%ld (%ld) |\n", current_number->num_full, current_number->key_end);
+            for (size_t j = 0; j < nodes_out_linear.count; j++) {
+                printf("\t > %ld ", nodes_out_linear.items[j]->num_full);
+                if (nodes_out_linear.items[j] != current_number->nodes_out.items[j]) {
+                    printf(" (DONT MATCH)");
+                }
+                printf("\n");
+            }
+        }
+    }
+}
 const char* filename = "source.txt";
 //const char* filename = "source_small.txt";
 int main()
 {
     Number_array number_array = parse_file(filename);
+    nodes_connect_by_key(number_array);
+
     Number_ptr_array longest_sequence = find_longest_sequence(number_array);
     print_sequence_pretty(longest_sequence);
     print_sequence(longest_sequence);
     printf("len: %zu\n", longest_sequence.count);
     //for (size_t i = 0; i < number_array.count; i++) {
     //    Number* current_number = &(number_array.items[i]);
-    //    printf("(%ld) %ld (%ld)\n", current_number->start_key, current_number->num_full, current_number->end_key);
+    //    printf("(%ld) %ld (%ld)\n", current_number->key_start, current_number->num_full, current_number->key_end);
     //}
     return 0;
 }
